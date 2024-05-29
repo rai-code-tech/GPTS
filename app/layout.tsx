6c661565f1daa93f1f93a885c5610255e764538d
maherk94 <maherkhial@gmail.com>
2024-05-29 19:39:31 +0200

    homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body classNameNameNameName={inter.classNameNameNameName}>
+        {children}
+      </body>
     </html>
   );
 }
