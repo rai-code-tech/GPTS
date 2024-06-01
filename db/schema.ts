1595b9e5bd970cd23cc6d8048c41b82c75ad4129
maherk94 <maherkhial@gmail.com>
2024-06-01 17:01:55 +0200

    modification schema and packge.json

--- db/schema.ts
+++ db/schema.ts
@@ -8,6 +8,6 @@ import {
 
 export const gpts = pgTable("gpts", {
   id: uuid("id").defaultRandom().primaryKey(),
-  name: varchar("name", { length: 100 }).notNull(),
-  description: varchar("description", { length: 2000 }).notNull(),
+  name: varchar("name", { length: 64 }).notNull(),
+  description: varchar("description", { length: 256 }).notNull(),
 });
